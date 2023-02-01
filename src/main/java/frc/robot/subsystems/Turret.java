e8808f475b9b15814ed960566758987564f2337b
Benwantland <bmwinstl@gmail.com>
2023-01-31 18:45:36 -0800

    Cleaned Up Unused Code
    Co-Authored-By: RiesPuff <123519245+RiesPuff@users.noreply.github.com>

--- src/main/java/frc/robot/subsystems/Turret.java
+++ src/main/java/frc/robot/subsystems/Turret.java
@@ -1,14 +1,11 @@
 package frc.robot.subsystems;
 
-import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.TalonSRXControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 
 public class Turret extends SubsystemBase {
-
-  private double motorValue = -1.0;
 
   // Inits motors
   private TalonSRX turntable = new TalonSRX(15);
