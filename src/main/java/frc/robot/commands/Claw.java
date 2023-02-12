8f1712e00371d18af36705d65127c597ace97390
Oakville Dynamics <reid.akers@icloud.com>
2023-02-12 12:31:28 -0600

    Created Pneumatic Control for Claw
    Co-Authored-By: RiesPuff <123519245+RiesPuff@users.noreply.github.com>

--- src/main/java/frc/robot/commands/Claw.java
+++ src/main/java/frc/robot/commands/Claw.java
@@ -1,16 +1,25 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot.commands;
 
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.Constants;
+import frc.robot.subsystems.PneumaticClaw;
+
 
 public class Claw extends CommandBase {
-  /** Creates a new PneumaticCommand. */
-  public Claw() {
-    // Use addRequirements() here to declare subsystem dependencies.
+  private boolean m_isClawOn = false;
+
+  //subsystems
+  private final PneumaticClaw m_Claw;
+
+   //controllers
+  private final Joystick controllerJoystick = new Joystick(Constants.controllerJoystickID);
+  public Claw(PneumaticClaw subsystem) {
+        m_Claw = subsystem;
+        addRequirements(m_Claw);
+
   }
+    
 
   // Called when the command is initially scheduled.
   @Override
@@ -18,8 +27,20 @@ public class Claw extends CommandBase {
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
-  public void execute() {}
+  public void execute() {
 
+    if (controllerJoystick.getRawButton(Constants.joystickButtonClawOn) == true) {
+      if (m_isClawOn){
+        m_Claw.clawOn();
+        m_isClawOn=true;
+        System.out.println("Claw On");
+      }else{ 
+        m_Claw.clawOff(); 
+        m_isClawOn=false;
+        System.out.println("Claw Off");
+      }
+    }
+  }
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {}
